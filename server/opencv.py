d33bd387ef1f053f27b4e8f0ee31e3d44c9ef43d
jean <forconijean@gmail.com>
2021-12-16 18:49:29 +0100

    image par image

--- server/opencv.py
+++ server/opencv.py
@@ -2,19 +2,21 @@ import cv2
 
 cap = cv2.VideoCapture("/Users/jean-jose/Desktop/keyframeJS/server/ressource/super.mp4")
 
-
+i=0
 while(cap.isOpened()):
 
   ret, frame = cap.read()
   if ret == True:
 
-    cv2.imshow('Frame', frame)
-
+    
+    cv2.imwrite("picture " + str(i) + ".jpg", frame )
+    i += 1
     if cv2.waitKey(25) & 0xFF == ord('q'):
       break
 
   else:
     break
+    
 
 cap.release()
 
