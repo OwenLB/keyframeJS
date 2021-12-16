531040960df509534a77d6fa16b972821d379a47
Owen <lebec.owen@yahoo.fr>
2021-12-16 18:12:04 +0100

    design front

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider/Slider';
+import SwiperCaroussel from './components/Swiper/SwiperCaroussel';
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar> 
+
+      <div className="params">
+        <div className="sildersContainer">
+          <div className="slider">
+            <h2>Slider 1</h2>
+            <Slider></Slider>
+            <h2>Slider 2</h2>
+            <Slider></Slider>
+            <h2>Slider 3</h2>
+            <Slider></Slider>
+          </div>
+        </div>
+        <ReactPlayer style={{marginTop: "35px"}} url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />        
+      </div>
+      <div className="button">
+        <button type="button">VALIDER</button>
+      </div>
+      <div className="keyframes">
+        <SwiperCaroussel></SwiperCaroussel>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div class="nav">
+                <div class="nav-header">
+                    {/* <img class="nav-img" src="https://media-exp1.licdn.com/dms/image/C4D0BAQGSs_Qfyuhh5g/company-logo_200_200/0/1537913246798?e=2159024400&v=beta&t=x9TlUePkHss2v_YsCbyDRf_g6oOIVFYzBx6F2dYiB0U"></img> */}
+                    <div class="nav-title">
+                    KeyframeJS
+                    </div>
+                </div> 
+            </div>
+        )
+    }
+}
